a760c9cab96a3e78cb446dae2ac535ab8ca8a4a7
Manish Acharya <manuacharya54@gmail.com>
2024-10-30 12:07:31 +0545

    added line selector to rcat

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -4,14 +4,29 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    for filename in args {
-        let file = File::open(filename)?;
-        let reader = BufReader::new(file);
+    let args: Vec<String> = env::args().collect();
+    let file_name = &args[1];
 
+    let file = File::open(file_name)?;
+    let reader = BufReader::new(file);
+
+    if args.len() == 2 {
         for line in reader.lines() {
             println!("{}", line?.blue());
         }
+    } else if args.len() == 3 {
+        let line_str = &args[2];
+        let line_num: u8 = line_str.trim().parse().unwrap();
+
+        let mut count = 0;
+        for line in reader.lines() {
+            count += 1;
+            if count == line_num {
+                println!("{}", line?.blue());
+                return Ok(());
+            }
+        }
     }
+
     Ok(())
 }
